0ea9a23298179d5a90a3b731c109bcd78005e026
Robinson_Xavier <robinson.webzen@gmail.com>
2022-09-04 06:19:25 -0300

    added get and post participants function

--- index.js
+++ index.js
@@ -17,7 +17,40 @@ mongoClient.connect(()=> {
     db = mongoClient.db('batepapouol')
 });
 
+app.get("/participants", async (req, res) => {
+
+    try {
+        const participants = await db.collection('participants').find().toArray();
+        res.send(participants)
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(500);
+    }
+});
+
+app.get("/messages", async (req, res) => {
+
+});
+
+app.post("/participants", async (req, res) => {
+
+    const {name} = req.body; //strings não vazio
+    const lastStatus = Date.now();
+    
+    try {
+        const response = await db.collection('participants').insertOne({name, lastStatus});
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(422);
+    }
+});
+
+app.post("/messages", async (req, res) => {
+
+    const {to, text, type} = req.body; //string não vazias, type só pode ser ´message'ou 'private-message'
+});
 
 app.listen(5000, () => {
     console.log('Server on');
-})
+});
